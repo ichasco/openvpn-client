49b329d5483a8f9fa07a3b8352a8bfbea1e7ebc8
ismaelchasco <ismael@ichasco.com>
2016-11-20 22:07:02 +0100

    Update certificados.sh

--- certificados.sh
+++ certificados.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-certname=$1
+cliente=$1
+IP=$2
 
-/bin/cp /etc/openvpn/clientes/template.conf /etc/openvpn/clientes/$certname.conf
+#Crear el archivo de configuracion en base a la plantilla
+/bin/cp /etc/openvpn/clientes/template.conf /etc/openvpn/clientes/$cliente.conf
 
-/bin/bash /etc/openvpn/easy-rsa/build-key --batch $certname
+#Crear los certificados
+/bin/bash /etc/openvpn/easy-rsa/build-key --batch $cliente
 
-/bin/echo '<ca>' >> /etc/openvpn/clientes/$certname.conf
-/bin/cat /etc/openvpn/easy-rsa/keys/ca.crt >> /etc/openvpn/clientes/$certname.conf
-/bin/echo '</ca>' >> /etc/openvpn/clientes/$certname.conf
+#Hacer el inline de los certificados
+/bin/echo '<ca>' >> /etc/openvpn/clientes/$cliente.conf
+/bin/cat /etc/openvpn/easy-rsa/keys/ca.crt >> /etc/openvpn/clientes/$cliente.conf
+/bin/echo '</ca>' >> /etc/openvpn/clientes/$cliente.conf
 
-/bin/echo '<cert>' >> /etc/openvpn/clientes/$certname.conf
-/bin/cat /etc/openvpn/easy-rsa/keys/$certname.crt >> /etc/openvpn/clientes/$certname.conf
-/bin/echo '</cert>' >> /etc/openvpn/clientes/$certname.conf
+/bin/echo '<cert>' >> /etc/openvpn/clientes/$cliente.conf
+/bin/cat /etc/openvpn/easy-rsa/keys/$cliente.crt >> /etc/openvpn/clientes/$cliente.conf
+/bin/echo '</cert>' >> /etc/openvpn/clientes/$cliente.conf
 
-/bin/echo '<key>' >> /etc/openvpn/clientes/$certname.conf
-/bin/cat /etc/openvpn/easy-rsa/keys/$certname.key >> /etc/openvpn/clientes/$certname.conf
-/bin/echo '</key>' >> /etc/openvpn/clientes/$certname.conf
+/bin/echo '<key>' >> /etc/openvpn/clientes/$cliente.conf
+/bin/cat /etc/openvpn/easy-rsa/keys/$cliente.key >> /etc/openvpn/clientes/$cliente.conf
+/bin/echo '</key>' >> /etc/openvpn/clientes/$cliente.conf
+
+/bin/echo 'ifconfig-push $IP 255.255.255.0' > /etc/openvpn/clientes/$cliente
