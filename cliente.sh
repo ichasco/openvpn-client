a878324cec8a81af18f1571d5bbf5f8f2bab5bf8
ismaelchasco <ismael@ichasco.com>
2016-11-22 13:40:09 +0100

    Update cliente.sh

--- cliente.sh
+++ cliente.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-cliente=$1
-IP=$2
+OPENVPN_RSA_DIR=/etc/openvpn/easy-rsa/
+OPENVPN_KEYS=$OPENVPN_RSA_DIR/keys
+
+#Introducir el cliente
+if [ -z "$1" ]
+	then echo -n "Nombre del nuevo cliente (CN): "
+	read -e cliente
+else
+	cliente=$1
+fi
+
+#Asegurarse de que no está en blanco
+if [ -z "$cliente" ]
+	then echo "Introduce el nombre de cliente."
+	exit
+fi
+
+
+
+#Establecer las variables
+cd $OPENVPN_RSA_DIR
+source ./vars > /dev/null
 
 #Crear el archivo de configuracion en base a la plantilla
 /bin/cp /etc/openvpn/clientes/template.conf /etc/openvpn/clientes/$cliente.conf
@@ -21,5 +41,3 @@ IP=$2
 /bin/echo '<key>' >> /etc/openvpn/clientes/$cliente.conf
 /bin/cat /etc/openvpn/easy-rsa/keys/$cliente.key >> /etc/openvpn/clientes/$cliente.conf
 /bin/echo '</key>' >> /etc/openvpn/clientes/$cliente.conf
-
-/bin/echo 'ifconfig-push $IP 255.255.255.0' > /etc/openvpn/clientes/$cliente
